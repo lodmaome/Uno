3ccc10e5e56644520bca6ba9c1a9e82d411717e4
lodmaome <leandro.donato@ymail.com>
2018-05-21 22:38:05 -0300

    Primeira versao

---
+++ app/src/main/java/uno/unisal/com/uno/IntroActivity.java
@@ -0,0 +1,28 @@
+package uno.unisal.com.uno;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class IntroActivity extends AppCompatActivity {
+
+    private ImageView botaoIntro;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        botaoIntro = (ImageView) findViewById(R.id.botaoIntroId);
+
+        botaoIntro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(IntroActivity.this, LoginActivity.class));
+            }
+        });
+    }
+}
